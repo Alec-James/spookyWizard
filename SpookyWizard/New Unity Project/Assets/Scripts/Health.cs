43d82c0aa872659b43224300e6f30cce3bd9faf1
southernCRICKET <peacock089@gmail.com>
2019-10-16 14:37:52 -0500

    Proj Remake and Added death screen
    Had to recreate my project due to errors. Magic and Health now have different scripts. When health reaches zero death screen displays and buttons all work.

---
+++ SpookyWizard/New Unity Project/Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Health : MonoBehaviour
+{
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
+    public Slider healthbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHealth = 20f;
+        CurrentHealth = MaxHealth;
+        healthbar.value = CalcHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+            DealDamage(6);
+    }
+
+    void DealDamage(float dmg)
+    {
+        CurrentHealth -= dmg;
+        healthbar.value = CalcHealth();
+        if (CurrentHealth <= 0)
+        {
+
+            Die();
+        }
+    }
+    float CalcHealth()
+    {
+        return CurrentHealth / MaxHealth;
+
+    }
+    void Die()
+    {
+        CurrentHealth = 0;
+        SceneManager.LoadScene("Death");
+    }
+}
