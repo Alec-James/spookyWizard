95763e8cd898cf706e4ac7cfc0f199e9b64b0801
marcusl975 <marcuslee975@gmail.com>
2019-10-16 10:40:17 -0500

    Boss Tracking/NavMesh Example
    Boss tracking of player funtionality

---
+++ SpookyWizardGame/Assets/scripts/BAttack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject iceShot;
+    public Transform LeftCast;
+    public GameObject player;
+    //layer = Gam
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) > 75)
+        {
+            shootIce();
+        }
+
+    }
+
+    private void shootIce()
+    {
+        GameObject tempBullet;
+        Transform player;
+        //player = playerPos;
+        tempBullet = Instantiate(iceShot) as GameObject;
+        tempBullet.transform.position = transform.position + LeftCast.position;
+    }
+}
