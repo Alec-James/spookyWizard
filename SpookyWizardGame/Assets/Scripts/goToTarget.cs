e01bd9ab1768effe46a382795c63013abf68f5e0
Alec-James <xalec2000@gmail.com>
2019-10-17 01:29:17 -0500

    boss and ooze tracking

--- SpookyWizardGame/Assets/Scripts/goToTarget.cs
+++ SpookyWizardGame/Assets/Scripts/goToTarget.cs
@@ -1,21 +1,167 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+﻿using UnityEngine;
+using System.Collections;
 
 public class goToTarget : MonoBehaviour
 {
-    public Transform target;
-    NavMeshAgent agent;
-    // Start is called before the first frame update
+    public Transform[] targets; // list of potential targets
+    private Transform target = null;
+    public float chaseSpeed = 2f;
+    public float wanderingSpeed = .75f;
+
+    UnityEngine.AI.NavMeshAgent nav;
+    public float headOffset = 1.5f; // the origin of this character is at the feet. we want to cast rays from the eyes
+    public float FOV = 110f; // how wide the ooze's field of view is in degrees
+
+    float lastStateChangeTime; //when was the last time we changed state
+    public float stateChangeInterval = 30f; // how often to change state
+
+    enum oozeState { chasing = 0, wandering = 1 }; // different states of the ooze
+    oozeState state = oozeState.wandering;
+
+    public static float normalPlayerWalkSpeed = 3f;
+    public static float normalPlayerRunSpeed = 6f;
+    public static float slowPlayerWalkSpeed = 1.5f;
+    public static float slowPlayerRunSpeed = 2f;
+    public static float timeOut = 5f;
+
+    //public FirstPersonController charScript = targets[0].GetComponent<FirstPersonController>();
+
+    // initialization
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        Random.seed = (int)System.DateTime.Now.Ticks;
+        nav = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
-    // Update is called once per frame
+    // try to find a target. if no target is found, wander. if a target is found, chase it.
     void Update()
     {
-        agent.SetDestination(target.position);
+
+            if (!Targeting())
+                Wandering();
+
+
+            if (state == oozeState.chasing && target)
+                Chasing();
+
+    }
+    //gets a ranom point on a circle around the zombie to decide where to wander to. OR stays idle for a little while
+    void Wandering()
+    {
+        if (Time.time > lastStateChangeTime + stateChangeInterval)
+        {
+            lastStateChangeTime = Time.time;
+            if (Random.Range(0, 100) > 50)
+            { // go to random destination
+                Vector2 vec2 = GetRandomOnCircle(Random.Range(1.0f, 4.0f));
+
+                nav.destination = new Vector3(transform.position.x + vec2.x, transform.position.y, transform.position.z + vec2.y);
+                print("wandering change " + gameObject.name + " " + nav.destination);
+                nav.speed = wanderingSpeed;
+
+            }
+            else
+            { // play an idle animation
+                nav.destination = transform.position;
+                print("idle change " + gameObject.name);
+                nav.speed = 0f;
+
+            }
+        }
+    }
+
+    //chase the target
+    void Chasing()
+    {
+
+        nav.destination = target.position; //set the target
+        nav.speed = chaseSpeed; // actual speed of movement
+        print("chasing" + target.tag);
+
+    }
+
+    // Check to see if the zombie can see a target. If so, return true. Else, return false.
+    bool Targeting()
+    {
+        if (!target)
+        {
+
+                Vector3 dir = targets[0].position - transform.position;
+                RaycastHit hit = new RaycastHit();
+                Debug.DrawLine(transform.position + Vector3.up * headOffset, targets[0].position + Vector3.up * headOffset, Color.red);
+
+                Debug.DrawLine(transform.position + Vector3.up * headOffset, (transform.position + Vector3.up * headOffset) + transform.forward);
+
+                //logic for modeling the oozes's field of view and make sure the potential target is not another enemy
+                if (Mathf.Acos(Vector3.Dot(transform.forward, Vector3.Normalize(dir))) < Mathf.Deg2Rad * (FOV / 2f)
+                   && Physics.Raycast(transform.position + Vector3.up * headOffset, dir, out hit)
+                       && hit.collider.gameObject.tag.Equals("Player")
+                       )
+                {
+
+                    target = targets[0];
+                    state = oozeState.chasing;
+                    print(hit.collider.gameObject.tag);
+                    return true;
+                }
+
+        
+        }
+        target = null;
+        return false;
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        // attack player and slow
+        if (other.collider.gameObject.tag.Equals("Player"))
+        {
+
+            //StartCoroutine(Pickup(other));
+
+        }
+    }
+
+
+    /*IEnumerator Pickup(Collider player)
+    {
+        //Debug.Log("ONE");
+
+
+
+        //apply player
+
+    charScript.walkSpeed = slowPlayerWalkSpeed;
+    charScript.runSpeed = slowPlayerRunSpeed;
+
+    yield return new WaitForSeconds(timeOut);
+
+    charScript.walkSpeed = normalPlayerWalkSpeed;
+    charScript.runSpeed = normalPlayerRunSpeed;
+
+}*/
+
+    Vector2 GetRandomOnCircle(float radius)
+    {
+
+        // initialize calculation variables
+        float _x = 0;
+        float _y = 0;
+        Vector2 _returnVector;
+
+        // convert degrees to radians
+        float angleRadians = Random.Range(-1f, 1f);
+        float angleRadians2 = Random.Range(-1f, 1f);
+
+        // get the 2D dimensional coordinates
+        _x = radius * Mathf.Cos(angleRadians);
+        _y = radius * Mathf.Sin(angleRadians2);
+
+        // derive the 2D vector
+        _returnVector = new Vector2(_x, _y);
+
+        // return the vector info
+        return _returnVector;
     }
 }
--- SpookyWizardGame/Assets/Scripts/playerStats.cs
+++ SpookyWizardGame/Assets/Scripts/playerStats.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class playerStats : MonoBehaviour
 {
+    int playerHealth;
+    int playerMana;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHealth = 100;
+        playerMana = 100;
+
     }
 
     // Update is called once per frame
