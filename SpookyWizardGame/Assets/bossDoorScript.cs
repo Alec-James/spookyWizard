1d608665d8455106fc578f393e3d4607710a7149
Collygon <collygon@gmail.com>
2019-12-02 14:28:40 -0600

    Map Completed - Version 1
    Map is basically completed, along with functions for other implementation. Still requires Slime monsters to be added, but the function to spawn them is already finished. Ask me for any questions.

---
+++ SpookyWizardGame/Assets/bossDoorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossDoorScript : MonoBehaviour
+{
+    public bool moveDoor = false;
+    private int continueMoving = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.moveDoor == true)
+        {
+            Debug.Log(this.moveDoor);
+            transform.Translate(Vector2.right * 1.2f * Time.deltaTime);
+            continueMoving += 1;
+        }
+        if (continueMoving >= 500)
+            stopDoor();
+            
+    }
+
+    public void openDoor()
+    {
+        this.moveDoor = true;
+    }
+
+    public void stopDoor()
+    {
+        this.moveDoor = false;
+    }
+}
