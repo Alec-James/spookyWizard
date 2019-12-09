a89d7b98317c58de80f9bda4602b0b5851a73073
Alec-James <xalec2000@gmail.com>
2019-12-09 04:00:47 -0600

    Capped framerate to 30fps

---
+++ SpookyWizardGame/Assets/frameRate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frameRate : MonoBehaviour
+{
+    // Lock framerate
+    void Awake()
+    {
+        Application.targetFrameRate = 30;
+    }
+
+
+}
